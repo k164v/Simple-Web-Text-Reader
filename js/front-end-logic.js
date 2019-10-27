ab5fc1ce57ccf7cba936df7d0356ffa0064781a6
km7 <wtwtwtj@gmail.com>
2019-10-27 17:50:58 +0800

    Updated the javascript

---
+++ js/front-end-logic.js
@@ -0,0 +1,19 @@
+var updateHTML = function(){
+	    document.getElementById("main_content") = 'asdasd';
+}
+
+var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;
+      text = text.split(/\n/);
+      newText = ''
+      text.forEach(function(element) {
+		  newText=newText+"<p>"+element+"</p>";
+	  });
+     document.getElementById("main_content").innerHTML=newText;
+    };
+    text = reader.readAsText(input.files[0]);
+  };
